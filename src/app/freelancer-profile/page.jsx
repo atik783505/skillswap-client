3e511e145df4bc9229ecd46e4c14b0386b75327f
atik783505 <atik13672@gmail.com>
2026-06-27 06:05:38 +0600

    ready deployment

---
+++ src/app/freelancer-profile/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FreelancerProfile = () => {
+    return (
+        <div>
+            <h2>this is freelancer Profile</h2>
+        </div>
+    );
+};
+
+export default FreelancerProfile;
